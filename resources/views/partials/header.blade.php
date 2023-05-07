111e8312fc475d6853128b94f1b04e244960bdad
broyettore <broyettore@gmail.com>
2023-05-07 13:19:33 +0200

    Project Scaffolding

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="container py-2 d-flex align-items-center justify-content-between">
+        <div class="title">
+           <h1 class="fs-4">
+            Laravel Train
+           </h1>
+        </div>
+        <nav>
+            <ul class="d-flex">
+                <li>
+                    <a href="#">
+                        Bookings
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Services
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Contact Us
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
